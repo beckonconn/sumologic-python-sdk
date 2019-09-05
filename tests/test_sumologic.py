c1df55bc54ad3db2baed697ed945dc746b10c1cb
Cesarone <1256576+beckonconn@users.noreply.github.com>
2019-09-05 12:48:03 -0500

    Added inital Redirect testing

--- tests/test_sumologic.py
+++ tests/test_sumologic.py
@@ -4,7 +4,8 @@ from sumologic.sumologic import *
 
 
 def test_sumologic_function_success(requests_mock):
-    requests_mock.get('https://api.sumologic.com/api/v1/collectors', text='test')
+    requests_mock.get('https://api.sumologic.com/api/v1/collectors', [{'status_code': 401}, {'status_code': 301, 'headers': {'Location': 'https://api.us2.sumologic.com/api/v1/collectors'}}])
+    requests_mock.get('https://api.us2.sumologic.com/api/v1/collectors', status_code=200)
     s, sumoEnd = sumologic(accessId='1234', accessKey='4567')
 
     assert isinstance(s, requests.sessions.Session)  # Test returning the Session Object
